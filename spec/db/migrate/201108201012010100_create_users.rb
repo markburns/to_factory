46508813ecbb446de9619b1b15429155dba5e743
Mark Burns <markthedeveloper@gmail.com>
2011-08-20 15:33:25 +0100

    extra column on db

--- spec/db/migrate/201108201012010100_create_users.rb
+++ spec/db/migrate/201108201012010100_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.column :name, :string, :null => false
+      t.column :email, :string, :null => false
     end
   end
 
