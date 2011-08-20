575fa274964b09fc96b8b9a5dbaa868051ea105b
Mark Burns <markthedeveloper@gmail.com>
2011-08-20 19:52:38 +0100

    added spork, wirble, active support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
-require 'active_record'
-require 'yaml'
-require 'fileutils'
-require 'ruby-debug'
-Dir['lib/**/*.rb'].each{|f| require f unless f=="lib/to_factory/version.rb"}
+require 'rubygems'
+require 'spork'
 
+Spork.prefork do
+  require 'active_record'
+  require 'yaml'
+  require 'fileutils'
+  require 'ruby-debug'
+  require 'active_support/core_ext/string'
+  require 'active_support/core_ext/hash'
+end
 
+Spork.each_run do
+  Dir['lib/**/*.rb'].each{|f| require f unless f=="lib/to_factory/version.rb"}
+end
