dc2b7b6388b4a2814f8b36a004757f637d9afa7f
Mark Burns <markthedeveloper@gmail.com>
2011-08-20 15:31:19 +0100

    generate attribute test passing

--- lib/to_factory/generator.rb
+++ lib/to_factory/generator.rb
@@ -1,9 +1,36 @@
 module ToFactory
+  class MissingActiveRecordInstance < Exception;end
+
   class Generator
+    attr_accessor :model_class, :object_instance
+
+    def initialize object=nil
+      return unless object
+
+      if object.is_a? ActiveRecord::Base
+        @model_class = object.class.to_s
+        @object_instance = object
+      else
+        @model_class = object.to_s
+      end
+    end
+
     def factory model
       out = "Factory #{model} do |#{model.to_s[0..0].downcase}|\n"
       out << yield if block_given?
       out << "end"
+    end
+
+    def attribute a
+      raise MissingActiveRecordInstance unless object_instance
+      value = object_instance.send a
+      value = "\"#{value}\"" if value.is_a? String
+      "  #{first_letter_of_class_name}.#{a} #{value}"
+    end
+
+    private
+    def first_letter_of_class_name
+      @model_class[0..0].downcase
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'active_record'
 require 'yaml'
 require 'fileutils'
+require 'ruby-debug'
 Dir['lib/**/*.rb'].each{|f| require f unless f=="lib/to_factory/version.rb"}
 
 
