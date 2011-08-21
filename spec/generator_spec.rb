0185f061b46e719dde9c64e04c5d33cfb93e5f6b
Mark Burns <markthedeveloper@gmail.com>
2011-08-21 10:45:06 +0100

    specs passing

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -25,14 +25,75 @@ describe ToFactory::Generator do
       end
 
       AR::Migrator.migrate('db/migrate')
-      AR::Base.connection.execute "VACUUM"
       class User < AR::Base; end
-    end
+   end
   end
 
-  it "initializes with an activerecord class" do
-    @generator = ToFactory::Generator.new User
-    @generator.model_class.should == "User"
+  before(:each) do
+    User.destroy_all
+    ActiveRecord::Base.connection.execute "delete from sqlite_sequence where name = 'users'"
+  end
+
+  context "with an active record class but no instance" do
+    before do
+      @generator = ToFactory::Generator.new User
+      User.create :id => 1, :name => 'Tom',   :email => 'blah@example.com',  :some_id => 7
+      User.create :id => 2, :name => 'James', :email => 'james@example.com', :some_id => 8
+    end
+
+    it "initializes" do
+      @generator.model_class.should == User
+    end
+
+    let(:user_factory_1) do
+      user_factory_1 = <<-eof
+Factory :user do |u|
+  u.email "blah@example.com"
+  u.name "Tom"
+  u.some_id 7
+end
+      eof
+      user_factory_1.chop
+    end
+    let(:user_factory_2) do
+      user_factory_2 = <<-eof
+Factory :user do |u|
+  u.email "james@example.com"
+  u.name "James"
+  u.some_id 8
+end
+      eof
+      user_factory_2.chop
+    end
+    context "looking up attributes" do
+      it "takes a key value for id" do
+        out = @generator.factory_for :id => 1
+        out.should == user_factory_1
+      end
+
+      it "takes an integer for id" do
+        out = @generator.factory_for 1
+        out.should == user_factory_1
+      end
+
+      it "takes a key value and does a lookup" do
+        out = @generator.factory_for :name => 'Tom'
+        out.should == user_factory_1
+
+        out = @generator.factory_for :name => 'James'
+        out.should == user_factory_2
+      end
+
+      it "takes multiple keys and does a lookup" do
+        out = @generator.factory_for :id => 1, :name => 'Tom'
+      end
+
+      context "with invalid attributes" do
+        it "raises an exception" do
+          lambda{@generator.factory_for :id => 1000}.should raise_error ActiveRecord::RecordNotFound
+        end
+      end
+    end
   end
 
   it "initializes with an activerecord instance" do
@@ -47,25 +108,59 @@ describe ToFactory::Generator do
   end
 
   it "generates the first line of the factory" do
-    @generator = ToFactory::Generator.new
-    f = @generator.factory User
+    @generator = ToFactory::Generator.new User
+    f = @generator.factory
     output = <<-eof
-Factory User do |u|
+Factory :user do |u|
 end
-eof
-    f.should ==  output[0..-2]
+    eof
+    f.should ==  output.chop
   end
+
 
   it "raises an exception without an AR object, when requesting attributes" do
     @generator = ToFactory::Generator.new User
-    lambda{@generator.attribute :foo}.should raise_error ToFactory::MissingActiveRecordInstance
+    lambda{@generator.factory_attribute :foo}.should raise_error ToFactory::MissingActiveRecordInstanceException
   end
 
-  it "generates the lines for attributes" do
-    User.create :name => "Jeff"
-    @generator = ToFactory::Generator.new User.first
-    @generator.attribute(:name).should == '  u.name "Jeff"'
+  context "with a user in the database" do
+    before do
+      User.create :name => "Jeff", :email => "test@example.com", :some_id => 8
+      @generator = ToFactory::Generator.new user
+    end
 
+    let(:user){ User.first}
+
+    it "generates lines for multiple the attributes" do
+      @generator.factory_attribute(:name). should == '  u.name "Jeff"'
+      @generator.factory_attribute(:email).should == '  u.email "test@example.com"'
+    end
+
+    let(:expected) do
+      expected = <<-eof
+Factory :user do |u|
+  u.email "test@example.com"
+  u.name "Jeff"
+  u.some_id 8
+end
+      eof
+      expected.chop
+    end
+
+    it "generates the full factory" do
+      @generator.factory_with_attributes.should == expected
+    end
+
+    it "adds the to_factory method to an active record object" do
+      ActiveRecord::Base.send :include, ToFactory
+      user.to_factory.should == expected
+    end
+
+    it "raises an error if you try to inlude in a non ActiveRecord object" do
+      class Egg;end
+
+      lambda{Egg.send :include, ToFactory}.should raise_error ToFactory::MustBeActiveRecordSubClassException
+    end
   end
 
 end
