10d93c842dc4a37ac53d0df128d2701318d9dce2
Mark Burns <markthedeveloper@gmail.com>
2014-12-21 10:46:38 +0100

    update debug related requires to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,21 @@ require 'active_support/core_ext/string'
 require 'active_support/core_ext/hash'
 require "to_factory"
 require "sqlite3"
-require "pry-byebug"
+
+begin
+  case RUBY_VERSION
+  when /JRUBY/i
+    nil
+  when /^1.8.7/
+    require "ruby-debug"
+  when /^1.9.\d/
+    require "debugger"
+  when /^2.\d/
+    require "pry-byebug"
+  end
+rescue LoadError
+  $stderr.puts "No debugger available for #{RUBY_VERSION}"
+end
 
 require "./spec/support/user"
 
@@ -18,7 +32,7 @@ RSpec.configure do |config|
   end
 
   config.expect_with :rspec do |expectations|
-     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
