edc1c44e899e6890635fa07f7c86d332f8832a36
Mark Burns <markthedeveloper@gmail.com>
2014-12-23 06:07:56 +0100

    remove unused implementation

--- lib/to_factory.rb
+++ lib/to_factory.rb
@@ -7,20 +7,6 @@ ActiveRecord::Base.send(:include, ToFactory)
 module ToFactory
   class << self
     def generate!(args)
-        if args.is_a?(Array)
-          return generate_all!(*args)
-        elsif args.is_a?(Hash)
-          return from_options!(args)
-        end
-    end
-
-    private
-
-    def generate_all!(*path)
-      AutoGenerator.new(path).all!
-    end
-
-    def from_options!(options)
       model_dir   = options.fetch(:models)
       factory_dir = options.fetch(:factories)
 
