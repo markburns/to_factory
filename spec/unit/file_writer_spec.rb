e1baae70e4e1abc1187897bdbc208d5fcb3b7f15
Mark Burns <markthedeveloper@gmail.com>
2014-12-24 05:44:05 +0000

    whitespace

--- spec/unit/file_writer_spec.rb
+++ spec/unit/file_writer_spec.rb
@@ -4,14 +4,15 @@ describe ToFactory::FileWriter do
     let!(:project) { double("project", :is_a? => true, :class => double(:name => "Project")) }
     let(:file_system  ) { double("file file_system" ) }
     let(:finder) { double("model finder") }
+    let(:factories) { {:user => "factory a", :project => "factory b"} }
 
     context "with a finder and a file system" do
       let(:generator) { ToFactory::FileWriter.new(finder, file_system) }
 
       before do
-        x(finder, :all).r [user, project]
-        x(file_system, :write, {:user => "factory a", :project => "factory b"})
-        x(generator,              :ToFactory          ).r("factory a", "factory b")
+        x(finder,      :all             ).r [user, project]
+        x(file_system, :write, factories)
+        x(generator,   :ToFactory       ).r("factory a", "factory b")
       end
 
       it "requests instances from the model finder and writes to the file file_system" do
