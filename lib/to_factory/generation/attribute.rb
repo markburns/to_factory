5a3aca2784ba0526e1eeba85fe7b37fb73540526
Mark Burns <markthedeveloper@gmail.com>
2015-01-08 17:12:56 +0100

    extract method

--- lib/to_factory/generation/attribute.rb
+++ lib/to_factory/generation/attribute.rb
@@ -19,22 +19,11 @@ module ToFactory
       def inspect_value(value, nested=false)
         formatted = case value
         when Time, DateTime
-          time = in_utc(value).strftime("%Y-%m-%dT%H:%M%Z").inspect
-          time.gsub(/UTC"$/, "Z\"").gsub(/GMT"$/, "Z\"")
+          inspect_time(value)
         when BigDecimal
           value.to_f.inspect
         when Hash
-          formatted = value.keys.inject([]) do |a, key|
-            formatted_key = inspect_value(key, true)
-            formatted_value = inspect_value(value.fetch(key), true)
-            a << "#{formatted_key} => #{formatted_value}"
-          end.join(', ')
-
-          if nested
-            "{#{formatted}}"
-          else
-            "({#{formatted}})"
-          end
+          inspect_hash(value, nested)
         when Array
           value.map{|v| inspect_value(v)}
         else
@@ -48,6 +37,26 @@ module ToFactory
         formatted
       end
 
+      private
+
+      def inspect_time(value)
+        time = in_utc(value).strftime("%Y-%m-%dT%H:%M%Z").inspect
+        time.gsub(/UTC"$/, "Z\"").gsub(/GMT"$/, "Z\"")
+      end
+
+      def inspect_hash(value, nested)
+        formatted = value.keys.inject([]) do |a, key|
+          formatted_key = inspect_value(key, true)
+          formatted_value = inspect_value(value.fetch(key), true)
+          a << "#{formatted_key} => #{formatted_value}"
+        end.join(', ')
+
+        if nested
+          "{#{formatted}}"
+        else
+          "({#{formatted}})"
+        end
+      end
       def in_utc(time)
         time.utc
       end
