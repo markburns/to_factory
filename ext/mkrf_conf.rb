04b21e00b94e6d43e7e302fb2e5c9b5a6ea7f195
Olga Grabek <olga.grabek@gmail.com>
2015-01-10 10:07:51 +0100

    add extention to install different gem version depending on ruby version

---
+++ ext/mkrf_conf.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'rubygems/command.rb'
+require 'rubygems/dependency_installer.rb' 
+begin
+  Gem::Command.build_args = ARGV
+  rescue NoMethodError
+end 
+inst = Gem::DependencyInstaller.new
+begin
+  if RUBY_VERSION < "1.9.3"
+    inst.install "to_factory", ">= 0.2.0", "< 1.0.0"
+  else
+    inst.install "to_factory", ">= 1.0.0"
+  end
+  rescue
+    exit(1)
+end 
+
+f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")   # create dummy rakefile to indicate success
+f.write("task :default\n")
+f.close
