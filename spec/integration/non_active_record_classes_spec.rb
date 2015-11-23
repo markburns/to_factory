8cdf4e07e3488e0eeba96b181fff3010e27a38a6
Mark Burns <markthedeveloper@gmail.com>
2015-11-23 13:59:26 +0900

    clarify spec with context

--- spec/integration/non_active_record_classes_spec.rb
+++ spec/integration/non_active_record_classes_spec.rb
@@ -16,23 +16,25 @@ describe "non active record classes" do
   let(:expected_project_file) { File.read "./spec/example_factories/project_with_header.rb" }
   let(:expected_inherited_project_file) { File.read "./spec/example_factories/inherited_project_with_header.rb" }
 
-  before do
-    ToFactory.models = "./spec/support/non_active_record"
-    ToFactory()
-  end
-
-  it "creates an ordinary factory correctly" do
-    expect(project_file).to match_sexp expected_project_file
-  end
-
-  context "STI" do
-    it "creates a single factory correctly" do
-      expect(inherited_project_file).to match_sexp expected_inherited_project_file
+  context "given a folder with non active record models in" do
+    before do
+      ToFactory.models = "./spec/support/non_active_record"
+      ToFactory()
     end
-  end
 
-  it "doesn't create other factories" do
-    count = Dir.glob("./tmp/factories/*.rb").length
-    expect(count).to eq 0
+    it "creates an ordinary factory correctly" do
+      expect(project_file).to match_sexp expected_project_file
+    end
+
+    context "STI" do
+      it "creates a single factory correctly" do
+        expect(inherited_project_file).to match_sexp expected_inherited_project_file
+      end
+    end
+
+    it "doesn't create other factories" do
+      count = Dir.glob("./tmp/factories/*.rb").length
+      expect(count).to eq 0
+    end
   end
 end
